6f3f1e47292a7d2b3ee8ad575b7668c99c411a80
Ronaq13 <raounaqsharma@gmail.com>
2017-01-12 18:24:04 +0530

    ng-repeate is working

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
-angular.module('myApp', ['ngMaterial'])
+angular.module('myApp', ['ngMaterial', 'ui.router'])
     .controller('myController', ['$scope', function($scope) {
         $scope.name = "Angular Done";
-    }])
-
-;
+        $scope.contents = [
+            { heading: 'Best Book', text: 'HC verma', imageSRC: 'images/book.jpeg' },
+            { heading: 'Evening', text: 'Will go to have my dish', imageSRC: 'images/dish.jpeg' }
+        ];
+    }]);
